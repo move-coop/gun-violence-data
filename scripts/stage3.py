73527727133859a972d25360d187e777dced4695
Martha Jones <marthao.jones15@gmail.com>
2020-02-07 22:40:21 -0500

    Update stage3.py
    added redshift credentials

--- scripts/stage3.py
+++ scripts/stage3.py
@@ -26,7 +26,14 @@ def main():
 
 
     #create an instance of redshift 
-    rs = Redshift()
+
+    username= os.environ['REDSHIFT_USERNAME']
+    password= os.environ['REDSHIFT_PASSWORD']
+    host=os.environ['REDSHIFT_HOST']
+    port=os.environ['REDSHIFT_DB']
+    dev=os.environ['REDSHIFT_PORT']
+
+    rs = Redshift(username=username, password=password, host=host, db=dev, port=port)
     
 
     STAGE2_GLOB = 'stage2.*.csv'
